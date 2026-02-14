6e566dd8075e2ef9e3e713cc4db83d3080287095
Shahboz1997 <sashabilov25@gmail.com>
2026-02-15 01:26:40 +0500

    Initial commit: IELTS Writing Checker with AI

--- prisma.config.js
+++ prisma.config.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
-import * as prismaConfig from "@prisma/config";
+import { defineConfig } from "@prisma/config";
 
-export default prismaConfig.defineConfig({
+export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    url: process.env.POSTGRES_URL_NON_POOLING,
+    // На Vercel эти переменные подтянутся из настроек проекта
+    url: process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL,
+    directUrl: process.env.POSTGRES_URL_NON_POOLING
   },
 });
