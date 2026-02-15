7fb0335e8285c594cc08655bbd60cd2edb07f8a9
Shahboz1997 <sashabilov25@gmail.com>
2026-02-15 22:41:38 +0500

    Initial commit: IELTS Writing Checker with AI

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = global.prisma || new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Строка с pooler для запросов
+      },
+    },
+  });
+};
+
+const prisma = global.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
