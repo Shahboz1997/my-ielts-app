ed80665baed835725ab2a5b015fa04f6ad5c49ae
Shahboz1997 <sashabilov25@gmail.com>
2026-02-17 09:09:59 +0500

    band booster

--- prisma.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  schema: './prisma/schema.prisma',
-  datasource: {
-    // Добавили &pgbouncer=true в конец
-    url: "postgresql://neondb_owner:npg_rsjJoU69zQup@ep-falling-heart-aikuyfnd-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&pgbouncer=true",
-  },
-});
